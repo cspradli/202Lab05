6be11efa3087d047751a707fcc08f45b65a04835
Caleb Spradlin <calebspradlin@dhcp-165-181.kar.secure.int.wireless.unca.edu>
2018-09-25 16:26:20 -0400

    added instance and declaration

---
+++ ArrayPalindrome.java
@@ -0,0 +1,11 @@
+public class ArrayPalindrome{
+    private ArrayStack<String> stack1;
+    private ArrayStack<String> stack2;
+    private ArrayStack<String> stack3;
+
+    public ArrayPalindrome(){
+        stack1 = new ArrayStack<>();
+        stack2 = new ArrayStack<>();
+        stack3 = new ArrayStack<>();
+    }
+}
